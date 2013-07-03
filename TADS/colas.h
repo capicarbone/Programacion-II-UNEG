115e16f4a56728713dcd3237c334f57d3ad44100
Carlos Pinelly <cpinelly@gmail.com>
2013-07-02 21:46:14 -0430

    Documentos de librerias para TADs

--- TADS/colas.h
+++ TADS/colas.h
@@ -1,13 +1,28 @@
+
+/*
+    Librería para el manejo de colas de enteros.
+    Creado para prácticas en las preparadurías de Programación II, UNEG.
+
+    Autor: Carlos Pinelly - cpinelly@gmail.com - @capicp
+    Version: 1.0
+    Liberado: 02/07/2013
+*/
+
 #ifndef COLAS_H_INCLUDED
 #define COLAS_H_INCLUDED
 
 #include "../Listas/listas.h"
 
+// Tipo de dato para una cola
+
 typedef struct COLA{
-    Nodo* ultimo;
-    Nodo* siguiente;
-    int longitud;
+    Nodo* ultimo;     // Apuntador al último elemento de la cola
+    Nodo* siguiente;  // Apuntador al siguiente elemento que saldrá de la cola
+    int longitud;     // Numero de elementos de la cola.
 } Cola;
+
+// Inicializa la estructura para una cola. Siempre debe llamarse antes de usarse una cola.
+// @param c Apuntador a la estructura de cola a ser inicializada.
 
 void inicializar_cola(Cola *c){
 
@@ -16,10 +31,18 @@ void inicializar_cola(Cola *c){
     c->longitud = 0;
 }
 
+// Indica si una cola no posee elemento alguno.
+// @param c Cola a evaluar
+// Retorna verdadero si la cola no posee elementos, de lo contrario, falso.
+
 int cola_vacia(Cola c){
 
     return (!c.siguiente );
 }
+
+// Inserta un elemento en la cola según el comportamiento definido para una cola.
+// @param c Referencia a la cola en la que se insertará el elemento
+// @param n Entero a ser insertado
 
 void encolar(Cola* c, int n){
 
@@ -35,6 +58,10 @@ void encolar(Cola* c, int n){
 
     c->longitud++;
 }
+
+// Devuelve el siguiente elemento en ser atendido de la cola.
+// @param c Cola en la que se desencolará.
+// Retorna el entero desencolado.
 
 int desencolar(Cola *c){
 
@@ -52,16 +79,25 @@ int desencolar(Cola *c){
     return i;
 }
 
-void longitud_cola(Cola c){
+// Indica el numero de elementos de una cola.
+// @param c Cola a verificar.
+
+int longitud_cola(Cola c){
     return c.longitud;
 }
+
+// En vista de la implementación sobre una lista de las colas deben liberarse
+// todos sus elementos. Esta función libera de memeoria todos los elementos de la cola.
+// @param c Cola a liberar
 
 void liberar_cola(Cola *c){
 
     liberar_lista( &(c->siguiente));
-    c->ultimo = NULL;
-    c->longitud = 0;
+    inicializar_cola(c);
 }
+
+// Muestra los elementos de una cola.
+// @param c Cola a mostrar.
 
 void mostrar_cola(Cola c){
 
--- TADS/pilas.h
+++ TADS/pilas.h
@@ -1,17 +1,36 @@
+
+/*
+    Librería para el manejo de pilas de enteros.
+    Creado para prácticas en las preparadurías de Programación II, UNEG.
+
+    Autor: Carlos Pinelly - cpinelly@gmail.com - @capicp
+    Version: 1.0
+    Liberado: 02/07/2013
+*/
+
 #ifndef PILAS_H_INCLUDED
 #define PILAS_H_INCLUDED
 
 #include "../Listas/listas.h"
 
+// Tipo de dato para una pila
+
 typedef struct PILA {
-    Nodo* tope;
-    int longitud;
+    Nodo* tope;         // Tope de la pila por donde ingresan y salen los elementos;
+    int longitud;       // Ńúmero de elementos de la pila.
 } Pila;
+
+// Indica si una pila no posee elemento alguno.
+// @param p Pila a evaluar
+// Retorna verdadero si la pila no posee elementos, de lo contrario, falso.
 
 int pila_vacia(Pila p){
 
     return (!p.tope);
 }
+
+// Inicializa la estructura para una pila. Siempre debe llamarse antes de usarse una cola.
+// @param p Apuntador a la estructura de pila a ser inicializada.
 
 void inicializar_pila( Pila *p){
 
@@ -19,12 +38,20 @@ void inicializar_pila( Pila *p){
     p->longitud = 0;
 }
 
+// Inserta un elemento en la pila según el comportamiento definido para una pila.
+// @param p Referencia a pa pila en la que se insertará el elemento
+// @param n Entero a ser insertado
+
 void empilar( Pila *p , int n){
 
     insertar_tope_m( &(p->tope), n );
     (p->longitud)++;
 
 }
+
+// Devuelve el siguiente elemento en ser extraido de la pila.
+// @param p CReferencia a la pila en la que se desempilará.
+// Retorna el entero desencolado.
 
 int desempilar(Pila *p){
 
@@ -43,15 +70,25 @@ int desempilar(Pila *p){
 
 }
 
+// Indica el numero de elementos de una pila.
+// @param p Pila a verificar.
+
 int longitud_pila(Pila p){
     return p.longitud;
 }
+
+// Muestra los elementos de una pila.
+// @param p Pila a mostrar.
 
 void mostrar_pila(Pila p){
 
     printf("\nn: %d \n", p.longitud);
     mostrar_lista(p.tope);
 }
+
+// En vista de la implementación sobre una lista de las pilas, deben liberarse
+// todos sus elementos. Esta función libera de memeoria todos los elementos de la pila.
+// @param p Pila a liberar
 
 void liberar_pila(Pila *p){
 
